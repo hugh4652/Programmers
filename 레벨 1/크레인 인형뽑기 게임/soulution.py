dbb49c8ce5c0337484657155c8749990dc7c61b2
hugh4652@naver.com <hugh4652@naver.com>
2020-08-01 08:29:15 +0900

    Lv1 Player

--- 레벨 1/크레인 인형뽑기 게임/soulution.py
+++ 레벨 1/크레인 인형뽑기 게임/soulution.py
@@ -1,29 +1,31 @@
-board = list(map(list, zip(*board)))
+def solution(board, moves):
+    answer = 0
+    board = list(map(list, zip(*board)))
+    
+    for i in range(0, len(board)):
+        while True:
+            if(board[i][0] != 0):
+                break
+            else:
+                board[i].pop(0)
 
-for i in range(0, len(board)):
-    while True:
-        if(board[i][0] != 0):
-            break
-        else:
-            board[i].pop(0)
+    stack = list()
 
-stack = list()
+    for num in moves:
+        try:
+            stack.append(board[num - 1].pop(0))
+        except:
+            continue
 
-for num in moves:
-    try:
-        stack.append(board[num - 1].pop(0))
-    except:
-        continue
+    for i in range(len(stack)):
+        basket = list()
+        for j in range(0, len(stack) - 1):
+            if(stack[j] != stack[j + 1]):
+                basket.append(stack[j])
+            else:
+                answer += 2
+                stack.pop(j)
+                stack.pop(j)
+                break
 
-for i in range(len(stack)):
-    basket = list()
-    for j in range(0, len(stack) - 1):
-        if(stack[j] != stack[j + 1]):
-            basket.append(stack[j])
-        else:
-            answer += 2
-            stack.pop(j)
-            stack.pop(j)
-            break
-        
-print(answer)
+    return answer
